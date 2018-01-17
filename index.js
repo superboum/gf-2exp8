77d0e785c371b72190de8a5824ff1e45a5b73240
Quentin Dufour <quentin@lyon.irisa.fr>
2018-01-17 17:05:32 +0100

    Fix divide

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ exports.divide = function(a, b) {
   var logA = logTable[a];
   var logB = logTable[b];
   // notice the plus below should be normal add but not GF(2^8) add!
-  var logA_plus_logB = (logA - logB) % 0xff;
-  return expTable[logA_plus_logB];
+  var logA_minus_logB = (logA - logB) % 0xff;
+  while(logA_minus_logB < 0) logA_minus_logB += 0xff
+  return expTable[logA_minus_logB];
 }
 
 
