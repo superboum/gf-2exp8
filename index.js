593e380daa8dcacb1328453b95c9c9b759c752e7
Quentin Dufour <quentin@lyon.irisa.fr>
2018-01-17 17:47:38 +0100

    Add lookup table to divide

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var expTable = require('./lookup-table/exp');
 var logTable = require('./lookup-table/log');
+var invTable = require('./lookup-table/inv');
 
 exports.add = function(a, b) {
   return a ^ b;
@@ -14,20 +15,12 @@ exports.multiply = function(a, b) {
   var logA = logTable[a];
   var logB = logTable[b];
   // notice the plus below should be normal add but not GF(2^8) add!
-  var logA_plus_logB = (logA + logB) % 0xff;
+  var logA_plus_logB = (logA + logB) % 0x11b;
   return expTable[logA_plus_logB];
 }
 
 exports.divide = function(a, b) {
-  if (a === 0x00 || b === 0x00) {
-    return 0x00
-  }
-  var logA = logTable[a];
-  var logB = logTable[b];
-  // notice the plus below should be normal add but not GF(2^8) add!
-  var logA_minus_logB = (logA - logB) % 0xff;
-  while(logA_minus_logB < 0) logA_minus_logB += 0xff
-  return expTable[logA_minus_logB];
+  return exports.multiply(a, invTable[b])
 }
 
 
