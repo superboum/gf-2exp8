341fe2f12a4a476966eafbda030cccf47ab6b7c3
Quentin Dufour <quentin@lyon.irisa.fr>
2018-01-17 16:39:20 +0100

    Add divide and substract

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ exports.add = function(a, b) {
   return a ^ b;
 }
 
+exports.sub = exports.add
+
 exports.multiply = function(a, b) {
   if (a === 0x00 || b === 0x00) {
     return 0x00
@@ -15,3 +17,16 @@ exports.multiply = function(a, b) {
   var logA_plus_logB = (logA + logB) % 0xff;
   return expTable[logA_plus_logB];
 }
+
+exports.divide = function(a, b) {
+  if (a === 0x00 || b === 0x00) {
+    return 0x00
+  }
+  var logA = logTable[a];
+  var logB = logTable[b];
+  // notice the plus below should be normal add but not GF(2^8) add!
+  var logA_plus_logB = (logA - logB) % 0xff;
+  return expTable[logA_plus_logB];
+}
+
+
